8acaa10373146cef348095001fac916c99ed9f30
Takuma Eguchi <j3317ujs@sit.ac.jp>
2024-12-10 16:06:58 +0900

    initial import

---
+++ 11/load_dataset.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from torchvision import datasets
+import torchvision.transforms.v2 as transforms
+import torch
+
+ds_train = datasets.FashionMNIST(
+    root='data',
+    train=True,
+    download=True,
+)
+
+print(f'numbers of datasets:{len(ds_train)}')
+
+image, target = ds_train[1]
+print(type(image),target)
+
+plt.imshow(image,cmap='gray_r')
+plt.title(target)
+plt.show()
+
+image = transforms.functional.to_image(image)
+image = transforms.functional.to_dtype(image,dtype=torch.float32,scale=True)
+print(image.shape,image.dtype)
+print(image.min(),image.max())
